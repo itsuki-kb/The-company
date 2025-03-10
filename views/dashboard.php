0eb9bdd9dfe37800d6fd71df2ecd95e1f3a08243
Itsuki <kobayashi_itsuki@macbookair.local>
2025-03-10 10:56:58 +0900

    replaced session full_name to username on dashboard

--- views/dashboard.php
+++ views/dashboard.php
@@ -28,7 +28,7 @@
             </a>
 
             <div class="navbar-nav">
-                <span class="navbar-text"><?= $_SESSION['full_name'] ?></span>
+                <span class="navbar-text"><?= $_SESSION['username'] ?></span>
                 <form action="../actions/logout.php" method="post" class="d-flex ms-2">
                     <button type="submit" class="text-danger bg-transparent border-0">Log out</button>
                 </form>
